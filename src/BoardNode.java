a810afff98c2948780199ad544959b2c972e34d5
Aakif Aslam <komodo009@gmail.com>
2020-05-09 19:05:45 -0400

    Added Win Conditions, and Random moves for Computer

--- src/BoardNode.java
+++ src/BoardNode.java
@@ -8,36 +8,51 @@ enum BoardSpot{
     Fox
 }
 
+class Location{
+    int row;
+    int col;
+    Location(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
+    public boolean equals(Location b){
+        return (this.row == b.row && this.col == b.col);
+    }
+}
+
 public class BoardNode {
     BoardNode parent;
-    BoardSpot[][] boardstate = new BoardSpot[8][8];
     ArrayList<BoardNode> children = new ArrayList<>();
+
+    Location[] GeeseLocs;
+    Location FoxLoc;
 
     //Initial constructor, make a starter board
     BoardNode(){
         this.parent = null;
 
-        //initialize checkerboard pattern
-        for(int row = 0; row < 8; row++){
-            for(int col = 0; col < 8; col++){
-               if ((row + col) % 2 == 0){
-                   boardstate[row][col] = BoardSpot.Invalid;
-               }
-               else{
-                   boardstate[row][col] = BoardSpot.Empty;
-               }
+        FoxLoc = new Location(7,2);
+
+        GeeseLocs = new Location[4];
+        for (int goose = 0; goose < 4; goose++) {
+            GeeseLocs[goose] = new Location(0, (goose*2) + 1);
+        }
+    }
+
+    char printSpot(Location s){
+        if(( s.row + s.col)%2 == 0){
+            return '□';
+        }
+        if (FoxLoc.equals(s)){
+            return 'f';
+        }
+        for (int goose = 0; goose < 4; goose++) {
+            if(GeeseLocs[goose].equals(s)){
+                return Integer.toString(goose + 1).charAt(0);
             }
         }
 
-        //fox is in second black square from the left on the bottom row of the board
-        boardstate[7][2] = BoardSpot.Fox;
-
-        //top row has geese on every valid square
-        for(int col = 0; col < 8; col++){
-            if (boardstate[0][col] == BoardSpot.Empty){
-                boardstate[0][col] = BoardSpot.Goose;
-            }
-        }
+        return '■';
     }
 
 
@@ -46,16 +61,105 @@ public class BoardNode {
         StringBuilder output = new StringBuilder();
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
-                output.append(switch (boardstate[row][col]) {
-                    case Goose -> "g";
-                    case Fox -> "f";
-                    case Empty -> "■";
-                    case Invalid -> "□";
-                });
+                Location loc = new Location(row, col);
+                output.append(printSpot(loc));
                 output.append(" ");
             }
             output.append("\n");
         }
         return output.toString();
+    }
+
+    public Boolean checkWinFox(){
+       return (FoxLoc.row == 0);
+    }
+
+    public boolean checkWinGoose(){
+        //check every direction
+        for(MoveDir dir: MoveDir.values()){
+            if (isValidMoveFox(dir)) return false;
+        }
+        return true;
+    }
+
+    public Boolean isValidMoveFox(MoveDir dir){
+        Location new_loc = switch (dir){
+            case ForwardLeft -> new Location(FoxLoc.row - 1, FoxLoc.col - 1);
+            case BackwardLeft -> new Location(FoxLoc.row + 1, FoxLoc.col - 1);
+            case ForwardRight -> new Location(FoxLoc.row - 1, FoxLoc.col + 1);
+            case BackwardRight -> new Location(FoxLoc.row + 1, FoxLoc.col + 1);
+        };
+
+        //check that it is valid
+        if (new_loc.row < 0 || new_loc.row > 7){
+            return false;
+        }
+        if (new_loc.col < 0 || new_loc.col > 7){
+            return false;
+        }
+
+        //check that no geese are there
+        for (int goose = 0; goose < 4; goose++) {
+            if(GeeseLocs[goose].equals(new_loc)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public Boolean moveFox(MoveDir dir){
+        if(!isValidMoveFox(dir)) return false;
+
+        FoxLoc = switch (dir){
+            case ForwardLeft -> new Location(FoxLoc.row - 1, FoxLoc.col - 1);
+            case BackwardLeft -> new Location(FoxLoc.row + 1, FoxLoc.col - 1);
+            case ForwardRight -> new Location(FoxLoc.row - 1, FoxLoc.col + 1);
+            case BackwardRight -> new Location(FoxLoc.row + 1, FoxLoc.col + 1);
+        };
+        return true;
+    }
+
+    public Boolean moveGoose(MoveDir dir, int mygoose){
+        Location new_loc = new Location(0,0);
+        Location goose_loc = GeeseLocs[mygoose];
+        switch (dir){
+            case ForwardLeft:
+                new_loc.row = goose_loc.row + 1;
+                new_loc.col = goose_loc.col - 1;
+                break;
+            case ForwardRight:
+                new_loc.row = goose_loc.row + 1;
+                new_loc.col = goose_loc.col + 1;
+                break;
+            case BackwardRight:
+                return false;
+            case BackwardLeft:
+                return false;
+        }
+
+        //check that it is valid
+        if (new_loc.row < 0 || new_loc.row > 7){
+            return false;
+        }
+        if (new_loc.col < 0 || new_loc.col > 7){
+            return false;
+        }
+
+        //check that no geese are there
+        for (int goose = 0; goose < 4; goose++) {
+            if(goose == mygoose) continue;
+            if(GeeseLocs[goose].equals(new_loc)) {
+                return false;
+            }
+        }
+
+        //check that fox aint there
+        if(FoxLoc.equals(new_loc)){
+            return false;
+        }
+
+        GeeseLocs[mygoose] = new_loc;
+        return true;
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,78 +1,192 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class main {
 
-	public static void main(String[] args) {
-		
-		Scanner scan = new Scanner(System.in);
-		
-		int playerID = 0;
-		int searchDepth = 0;
+    public static void main(String[] args) {
 
-		//ask the user if they want to be fox or goose
-		System.out.println("Welcome to Fox and Geese!");
-		System.out.println("Do you want to play as the Fox or the Geese?(enter 1 for Fox and 2 for Geese): ");
-		
-		//scan the user input
-		if(scan.hasNextInt()){	
-			playerID = scan.nextInt();
-		}
-		
-		//ask the user for search depth
-		System.out.println("What is the depth of seach allowed: ");
-		
-		//scan the user input
-		if(scan.hasNextInt()){	
-			searchDepth = scan.nextInt();
-		}
+        Random ran = new Random();
 
-		//create player objects - fox or geese
-		//create computer objects - fox or geese
-		
-		boolean gameOver = false;
-		while(!gameOver){
-			int playerMove = 0;
-			
-			//give player the menu of choices for input
-			if(playerID == 1){
-				//fox
-				System.out.println("Current available moves:\n" +
-						"1. right forward\n" +
-						"2. left forward\n" +
-						"3. right backward\n" +
-						"4. left backward");
-			}
-			else{
-				//geese
-				System.out.println("Current available moves:\n" +
-					   "1. right goose 1\n" +
-		         	   "2. left goose 1\n" +
-		    		   "3. right goose 2\n" +
-		         	   "4. left goose 2\n" +
-		    		   "5. right goose 3\n" +
-		         	   "6. left goose 3\n" +
-		    		   "7. right goose 4\n" +
-		         	   "8. left goose 4");
-			}
-			
-			//scan for input
-			//scan the user input
-			if(scan.hasNextInt()){	
-				playerMove = scan.nextInt();
-			}
-			
-			//validate the move
-			
-			//make the move on the board
-			
-			//print board
-			
-			//computer do a minimax search and make the move
-			
-			//print the computer move
-			
-			//print board
-			
-		}
-	}
+        Scanner scan = new Scanner(System.in);
+
+        int playerID = 0;
+        int searchDepth = 0;
+
+        //ask the user if they want to be fox or goose
+        System.out.println("Welcome to Fox and Geese!");
+        System.out.println("Do you want to play as the Fox or the Geese?(enter 1 for Fox and 2 for Geese): ");
+
+        //scan the user input
+        if (scan.hasNextInt()) {
+            playerID = scan.nextInt();
+        }
+
+        //ask the user for search depth
+        System.out.println("What is the depth of seach allowed: ");
+
+        //scan the user input
+        if (scan.hasNextInt()) {
+            searchDepth = scan.nextInt();
+        }
+
+        //create player objects - fox or geese
+        //create computer objects - fox or geese
+
+        BoardNode bn = new BoardNode();
+
+        System.out.println(bn);
+
+        boolean gameOver = false;
+        while (!gameOver) {
+            //--------------------------------PLAYER MOVING--------------------------------
+            int playerMove = 0;
+
+            //give player the menu of choices for input
+            if (playerID == 1) {
+                //fox
+                System.out.println("Current available moves:\n" +
+                        "1. right forward\n" +
+                        "2. left forward\n" +
+                        "3. right backward\n" +
+                        "4. left backward");
+            } else {
+                //geese
+                System.out.println("Current available moves:\n" +
+                        "1. right goose 1\n" +
+                        "2. left goose 1\n" +
+                        "3. right goose 2\n" +
+                        "4. left goose 2\n" +
+                        "5. right goose 3\n" +
+                        "6. left goose 3\n" +
+                        "7. right goose 4\n" +
+                        "8. left goose 4");
+            }
+
+            //scan for input
+
+            //Make a move on the board
+            boolean moveMade = false;
+            while (!moveMade) {
+                if (scan.hasNextInt()) {
+                    playerMove = scan.nextInt();
+                    if (playerMove == 0) {
+                        gameOver = true;
+                        break;
+                    }
+                }
+                if (playerID == 1) {
+                    MoveDir playerDirection;
+                    //fox
+                    playerDirection = switch (playerMove) {
+                        case 1 -> MoveDir.ForwardRight;
+                        case 2 -> MoveDir.ForwardLeft;
+                        case 3 -> MoveDir.BackwardRight;
+                        case 4 -> MoveDir.BackwardLeft;
+                        default -> MoveDir.ForwardRight;
+                    };
+
+                    moveMade = bn.moveFox(playerDirection);
+
+                } else {
+                    MoveDir playerDirection;
+                    int goose;
+
+                    if (playerMove % 2 == 0) {
+                        playerDirection = MoveDir.ForwardLeft;
+                    } else {
+                        playerDirection = MoveDir.ForwardRight;
+                    }
+                    goose = (playerMove + 1) / 2 - 1;
+
+                    moveMade = bn.moveGoose(playerDirection, goose);
+                }
+
+                if(!moveMade){
+                    System.out.println("That move is invalid, please select another move");
+                }
+
+            }
+
+            //print board
+            System.out.println(bn);
+
+            //check for win conditions
+            if(bn.checkWinFox()){
+                System.out.println("The Fox won!");
+                gameOver = true;
+                break;
+            }
+            if(bn.checkWinGoose()){
+                System.out.println("Geese have won");
+                gameOver = true;
+                break;
+            }
+
+            //--------------------------------COMPUTER MOVING--------------------------------
+            //computer do a minimax search and make the move
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if (playerID == 1){
+                //computer plays as goose
+                int goose = ran.nextInt(4);
+                int dirChoice = ran.nextInt(2);
+                MoveDir dir = switch(dirChoice){
+                    case 0 ->MoveDir.ForwardRight;
+                    case 1 -> MoveDir.ForwardLeft;
+                    default -> MoveDir.ForwardRight;
+                };
+                while(!bn.moveGoose(dir, goose)){
+                    goose = ran.nextInt(4);
+                    dirChoice = ran.nextInt(2);
+                    dir = switch(dirChoice){
+                        case 0 ->MoveDir.ForwardRight;
+                        case 1 -> MoveDir.ForwardLeft;
+                        default -> MoveDir.ForwardRight;
+                    };
+                }
+
+                System.out.println("Computer moved goose " + (goose + 1) + " direction " + dir.name());
+
+            } else{
+               int dirChoice = ran.nextInt(4);
+                MoveDir dir = switch(dirChoice){
+                    case 0 ->MoveDir.ForwardRight;
+                    case 1 -> MoveDir.ForwardLeft;
+                    case 2 -> MoveDir.BackwardLeft;
+                    case 3 -> MoveDir.BackwardRight;
+                    default -> MoveDir.ForwardRight;
+                };
+                while(!bn.moveFox(dir)){
+                    dirChoice = ran.nextInt(4);
+                    dir = switch(dirChoice){
+                        case 0 ->MoveDir.ForwardRight;
+                        case 1 -> MoveDir.ForwardLeft;
+                        case 2 -> MoveDir.BackwardLeft;
+                        case 3 -> MoveDir.BackwardRight;
+                        default -> MoveDir.ForwardRight;
+                    };
+                }
+                System.out.println("Computer moved the fox " + dir.name());
+            }
+
+            System.out.println(bn);
+
+            //check for win conditions
+            if(bn.checkWinFox()){
+                System.out.println("The Fox won!");
+                System.out.println("You lost to a computer haha noob");
+                gameOver = true;
+                break;
+            }
+            if(bn.checkWinGoose()){
+                System.out.println("Geese have won");
+                gameOver = true;
+                break;
+            }
+        }
+    }
 }
